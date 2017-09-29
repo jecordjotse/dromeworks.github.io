aa97183b1af20d06ea2355e01318b4dbe4c07278
jecordjotse <jeromecordjotse@gmail.com>
2017-09-29 15:05:52 +0000

    php testing

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+
+include('./gen/controller.gen.php'); 
+include('./gen/connector.gen.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<?php include('./inc/head.inc.php') ?>
+</head>
+<body>
+<!-- <Header start-->
+<?php include('./inc/header.inc.php'); ?>
+<!-- Header end/> -->
+
+<!-- <content start-->
+<?php include('./inc/content.inc.php'); ?>
+<!-- content end/>-->
+<?php include('./inc/footer.inc.php'); ?>
+</body>
+</html>
+
